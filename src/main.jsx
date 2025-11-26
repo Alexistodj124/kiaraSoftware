a68121105823a3393e24a6bffadd88f960f3184d
Alexistodj124 <55223401+Alexistodj124@users.noreply.github.com>
2025-11-26 17:01:56 -0500

    Set basename for RouterProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <CssBaseline />
-      <RouterProvider router={router} />
+      <RouterProvider router={router} basename="/kiara" />
     </AuthProvider>
   </React.StrictMode>
 )
